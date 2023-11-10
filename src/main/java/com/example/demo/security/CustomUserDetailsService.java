c71267fd803f8078d70402bb6bfadf323d6dc486
chis-facultate <147166036+chis-facultate@users.noreply.github.com>
2023-11-10 16:00:41 +0200

    autentificare cu parola criptata

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -2,31 +2,25 @@ package com.example.demo.security;
 
 import com.example.demo.persistence.User;
 import com.example.demo.repository.UserRepository;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
-    //private final PasswordEncoder passwordEncoder;
-/*
+    private final PasswordEncoder passwordEncoder;
+
     public CustomUserDetailsService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
-    */
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> optionalUser = userRepository.findByLoginUsername(username);
@@ -37,7 +31,8 @@ public class CustomUserDetailsService implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(
                 optionalUser.get().getLoginUsername(),
                 optionalUser.get().getPasswordHash(),
-                Arrays.asList(new SimpleGrantedAuthority("USER"))
+                //Arrays.asList(new SimpleGrantedAuthority("USER"))
+                new ArrayList<>()
         );
     }
 }
--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -2,29 +2,20 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(
+//@EnableGlobalMethodSecurity( // Adnotare hibernate pentru useri cu roluri diferite
 //        prePostEnabled = true,
 //        securedEnabled = true,
-        jsr250Enabled = true)
+//       jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private CustomUserDetailsService customUserDetailsService;
 
@@ -32,35 +23,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.customUserDetailsService = customUserDetailsService;
     }
 
-    /*
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-     */
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()// This line disables Cross-Site Request Forgery protection. it is often disabled for stateless authentication mechanisms like token-based authentication. It allows HTTP requests without the need for CSRF tokens
+        http.csrf().disable()// disables Cross-Site Request Forgery protection.
+                // It is often disabled for stateless authentication mechanisms like token-based authentication.
+                // It allows HTTP requests without the need for CSRF tokens.
+                // *** desi autentificarea e cu session trebuie dezactivat oricum.
                 .authorizeRequests() //This method indicates that you're about to configure authorization rules for different URL patterns
                 .antMatchers("/login").permitAll() // no need to auth to access the endpoint
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated();// for any other request (anyRequest), the user must be authenticated to access it. In other words, to access any URL not explicitly configured with "permitAll," users must be logged in.
     }
 
-    @Bean
-    public AuthenticationSuccessHandler authenticationSuccessHandler() {
-        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-        successHandler.setTargetUrlParameter("redirectTo"); // Redirect to the "redirectTo" parameter if available
-        successHandler.setDefaultTargetUrl("/home"); // Default redirect URL to "/home"
-        return successHandler;
-    }
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService)
-                //.passwordEncoder(passwordEncoder());
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
